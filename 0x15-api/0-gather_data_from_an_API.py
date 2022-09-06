806d6f1786a878496d039c26fe8b56cf4c64cb17
ezesilva95 <4381@holbertonschool.com>
2022-09-06 10:49:10 -0700

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -22,13 +22,14 @@ if __name__ == "__main__":
 
     for task in todo.json():
         if task.get('userId') == int(user_id):
-            TOTAL_NUMBER_OF_TASKS +=1
+            TOTAL_NUMBER_OF_TASKS += 1
             if task.get('completed') is True:
                 NUMBER_OF_DONE_TASKS += 1
                 TASK_TITLE.append(task.get('title'))
 
     print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
                                                           NUMBER_OF_DONE_TASKS,
-                                                          TOTAL_NUMBER_OF_TASKS))
+                                                          TOTAL_NUMBER_OF_TASKS
+                                                          ))
     for tasks in TASK_TITLE:
         print("\t {}".format(tasks))
