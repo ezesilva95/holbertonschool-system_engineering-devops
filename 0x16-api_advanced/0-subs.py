84d631ef83e25aa1089aea5a50073557d391a6a2
ezesilva95 <4381@holbertonstudents.com>
2022-09-21 17:52:19 +0000

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,6 +12,6 @@ def number_of_subscribers(subreddit):
                             allow_redirects=False)
 
     if sub_info.status_code >= 300:
-            return 0
+        return 0
 
     return sub_info.json().get("data").get("subscribers")
